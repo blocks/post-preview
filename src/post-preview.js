15ccbb7dd8380732506e0551bf71405e5b953f9c
Josiah Sprague <josiah.sprague@gmail.com>
2015-10-08 11:12:42 -0400

    minor initial dev updates

--- src/post-preview.js
+++ src/post-preview.js
@@ -1,13 +1,42 @@
-var qs = require('querystring');
-var template = require('./post-preview.hbs');
+var _ = require('underscore');
 var SolidusClient = require('solidus-client');
-var params = qs.parse(window.location.search.split('?').pop());
+var params = require('querystring').parse(window.location.search.split('?').pop());
 
 module.exports = PostPreview;
 
 function PostPreview (options) {
+  var isPreviewActive = params._wp_json_nonce && params.is_preview;
+  var hasRequiredOptions = options.domain && options.template && options.element && options.post_id;
+  if ( !isPreviewActive || !hasRequiredOptions ) {
+    console.warn(isPreviewActive?'Missing required options.':'Preview not active');
+    return;
+  }
   if (!(this instanceof PostPreview)) return new PostPreview(options);
   var instance = this;
+  var previewClient = new SolidusClient();
+  var preprocessor = function(context) {
+    var lastRevision = context.resources.revisions[0];
+    context.resources.wordpress = _.extend(context.resources.wordpress, lastRevision);
+    if (typeof options.preprocessor == 'function') { context = options.preprocessor(context); }
+    return context.resources.wordpress;
+  }
+  var view = {
+    resources: {
+      wordpress: {
+        url: options.domain+'/wp-json/posts/'+options.post_id,
+        with_credentials: true,
+        query: { '_wp_json_nonce': params._wp_json_nonce }
+      },
+      revisions: {
+        url: options.domain+'/wp-json/posts/'+options.post_id+'/revisions',
+        with_credentials: true,
+        query: { '_wp_json_nonce': params._wp_json_nonce }
+      }
+    },
+    preprocessor: preprocessor,
+    template: options.template,
+    template_options: options.template_options || {}
+  };
 
   if (options.element.jquery) {
     instance.element = options.element[0];
@@ -17,50 +46,11 @@ function PostPreview (options) {
     instance.element = document.querySelector(options.element);
   }
 
-  var previewClient = new SolidusClient();
+  previewClient.render(view).end(function(err, html) {
+    if (err) console.log(err);
+    if (html) {
+      instance.element.outerHTML = html;
+    }
+  });
 
-  options = _setDefaults(options, instance);
-
-  if (options.resource) {
-
-    var preprocessor = function(context) {
-      // Render the preview resource directly, allows re-use of server-side templates
-      context = context.resources.preview;
-      return context;
-    };
-
-    var view = {
-      resources: {
-        preview: {
-          url: options.resource,
-          with_credentials: true,
-          query: {'_wp_json_nonce': params._wp_json_nonce}
-        }
-      },
-      preprocessor: preprocessor,
-      template: instance.template
-    };
-
-    previewClient.render(view).end(function(err, html) {
-      if (html) {
-        instance.element.outerHTML = html;
-        instance.element = document.getElementById(options.id);
-      }
-    });
-  } else {
-    console.warn('No resource defined.');
-  }
-
-}
-
-//Private Functions
-function _setDefaults(options, instance) {
-
-  if (typeof options.template === 'function') {
-    instance.template = options.template;
-  } else {
-    instance.template = template;
-  }
-
-  return options;
 }
